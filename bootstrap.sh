19dd2e07c1cd8316e53909c3bce15bca62f607f8
Sergey Nuzdhin <ipaq.lw@gmail.com>
2015-10-30 14:44:56 +0100

    NFS mounting of virtual machine, use build images of core and web

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,24 @@
 # /bin/bash
 
+echo "Install docker machine NFS driver for proper live update \n"
+sudo curl -o /usr/local/bin/docker-machine-nfs https://raw.githubusercontent.com/adlogix/docker-machine-nfs/master/docker-machine-nfs.sh
+sudo chmod +x /usr/local/bin/docker-machine-nfs
+
 MACHINE_NAME=MonetarioDev
 
 echo "\nCreating docker-machine with vagrant backend...\n"
 docker-machine create -d virtualbox $MACHINE_NAME
 
+docker-machine-nfs $MACHINE_NAME
+
 echo "\nActivating newly created machine...\n"
 eval "$(docker-machine env ${MACHINE_NAME})"
 
 echo "\nDownloading core API repository...\n"
-git clone git@github.com:monetario/core.git api/core
+git clone git@github.com:monetario/core.git core
 
 echo "\nDownloading basic web client...\n"
-git clone git@github.com:monetario/web.git web/web
+git clone git@github.com:monetario/web.git web
 
 echo "\nBuilding docker containers...\n"
 docker-compose build
